177bb493b69020707f87921f4e25c6661bc89d93
Zhu Liang <zhuliangg11@gmail.com>
2016-10-22 22:07:02 +0800

    implement fact filtering

--- ml/hw3/hw3-qasets.py
+++ ml/hw3/hw3-qasets.py
@@ -4,6 +4,7 @@ import numpy.random as nr
 from sklearn.svm import SVC
 from sklearn import preprocessing
 
+import itertools
 import math
 import re
 import word2vec
@@ -11,8 +12,10 @@ import word2vec
 nr.seed(3244)
 
 # load word2vec
-w2vDimension = 100
 w2vModel = word2vec.load('text8.bin')
+# w2vModel = word2vec.load('train-100.bin')
+w2vDimension = w2vModel.vectors.shape[1]
+print('word2vec dimension: ' + str(w2vDimension) + '\n')
 
 # load datasets code
 def parseLineTraining(line):
@@ -43,24 +46,41 @@ def getVectorFromWord(word):
         return w2vModel[word]
     else:
         # out of dictionary words have zero weights
-        return np.zeros(100)
+        return np.zeros(w2vDimension)
 
-def addTrainingData(existingFacts, answers):
-    vectorWords = [getVectorFromWord(w) for w in existingFacts]
+def isFactRelevant(fact, questionWords):
+    return len(set.intersection(set(fact), set(questionWords))) > 0
+
+def getRelevantFacts(existingFacts, questionWords):
+    relevantFacts = [i for i in existingFacts if isFactRelevant(i, questionWords)]
+    # flatten list
+    return list(itertools.chain.from_iterable(relevantFacts))
+
+def addTrainingData(questionFacts, questionWords, answers):
+    # print(questionFacts)
+    # print(questionWords)
+    # print(getRelevantFacts(questionFacts, questionWords))
+    # print('-------')
+    vectorWords = [getVectorFromWord(w) for w in getRelevantFacts(questionFacts, questionWords)]
     dataTrain.append(np.sum(vectorWords, axis=0))
     labelTrain.append(answers)
 
 def loadTrainingData(f):
     existingFacts = []
+    existingFactsWithQuestions = []
     for line in f:
         index, isQuestion, words, answers = parseLineTraining(line)
         if index == '1':
             existingFacts = []
+            existingFactsWithQuestions = []
         elif isQuestion:
-            existingFacts.extend(words)
-            addTrainingData(existingFacts, answers)
+            existingFactsWithQuestions.append(words)
+            questionFacts = existingFacts[:]
+            questionFacts.append(words)
+            addTrainingData(questionFacts, words, answers)
         else:
-            existingFacts.extend(words)
+            existingFactsWithQuestions.append(words)
+            existingFacts.append(words)
 
 with open('train.txt', 'r') as f:
 # with open('train-s.txt', 'r') as f:
@@ -74,6 +94,7 @@ def svmTrain(dataTrain, labelTrain, cost, kernel, gamma, degree):
     # print(labelTrain)
     # labelTrain = le.transform(labelTrain)
     # print(labelTrain)
+    # clf = SVC(cost, kernel, degree, gamma, class_weight="balanced")
     clf = SVC(cost, kernel, degree, gamma)
     clf.fit(dataTrain, labelTrain)
     return (clf, np.sum(clf.n_support_))
@@ -102,8 +123,10 @@ def parseLineTesting(line):
         words.append(re.sub(r'[^\w]', '', x))
     return (index, isQuestion, words)
 
-def getSvmOutput(existingFacts, svmModel):
-    vectorWords = [getVectorFromWord(w) for w in existingFacts]
+def getSvmOutput(questionFacts, existingFactsWithQuestions, questionWords, svmModel):
+    vectorWords = [getVectorFromWord(w) for w in getRelevantFacts(questionFacts, questionWords)]
+    # flatten existing facts list
+    flattenedExistingFacts = list(itertools.chain.from_iterable(existingFactsWithQuestions))
     answerStr = svmModel.predict(np.sum(vectorWords, axis=0).reshape(1,-1))
     # print(answerStr)
     answers = answerStr[0].split(',')
@@ -111,30 +134,31 @@ def getSvmOutput(existingFacts, svmModel):
         answer = answers[0]
         if answer == 'nothing':
             return str(-1)
-        elif answer in existingFacts:
+        elif answer in flattenedExistingFacts:
             # print(answer + '\n')
-            # print(existingFacts)
+            # print(flattenedExistingFacts)
             # print('\n')
-            return str(existingFacts.index(answer) + 1)
+            return str(flattenedExistingFacts.index(answer) + 1)
         else:
             # print('cannot find predicted word ' + answer + ' in facts:\n')
-            # print(existingFacts)
+            # print(flattenedExistingFacts)
             # print('\n')
             return str(-1)
     else:
         indices = []
         for answer in answers:
-            if answer in existingFacts:
-                indices.append(existingFacts.index(answer) + 1)
+            if answer in flattenedExistingFacts:
+                indices.append(flattenedExistingFacts.index(answer) + 1)
         return " ".join(str(x) for x in sorted(indices))
 
 def printOutput(fo, storyId, questionId, output):
     fo.write(str(storyId) + '_' + str(questionId) + ',' + output + '\n')
 
 def svmTest(f, svmModel):
-    fo = open('test-output.txt', 'w')
+    fo = open('test-output-' + kernel + '-filter.txt', 'w')
     fo.write("textID,sortedAnswerList" + '\n')
     existingFacts = []
+    existingFactsWithQuestions = []
     storyId = 0
     questionId = 1
     for line in f:
@@ -143,15 +167,19 @@ def svmTest(f, svmModel):
             storyId += 1
             questionId = 1
             existingFacts = []
+            existingFactsWithQuestions = []
         if isQuestion:
-            existingFacts.extend(words)
-            output = getSvmOutput(existingFacts, svmModel)
+            existingFactsWithQuestions.append(words)
+            questionFacts = existingFacts[:]
+            questionFacts.append(words)
+            output = getSvmOutput(questionFacts, existingFactsWithQuestions, words, svmModel)
             # if storyId == 1 and questionId == 1:
             #     print(existingFacts)
             printOutput(fo, storyId, questionId, output)
             questionId += 1
         else:
-            existingFacts.extend(words)    
+            existingFactsWithQuestions.append(words)
+            existingFacts.append(words)    
 
 # printing util function
 def printResult(kernel, cost, totalSV, trainAccuracy, testAccuracy):
@@ -162,7 +190,8 @@ def printResult(kernel, cost, totalSV, trainAccuracy, testAccuracy):
     print("Test Accuracy: " + str(testAccuracy)+"\n")
 
 cost = 1
-kernel = 'poly'
+kernel='rbf'
+# kernel = 'poly'
 degree = 3
 gamma = 'auto'
 
