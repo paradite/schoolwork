c084c511d1e02118141983956939a0fc820960e1
paradite <zhuliangg11@gmail.com>
2016-11-01 22:08:00 +0800

    update word2vec train model

--- ml/hw3/train-word2vec.py
+++ ml/hw3/train-word2vec.py
@@ -1,7 +1,11 @@
 import word2vec
 
-word2vec.word2phrase('train-cleaned.txt', 'train-phrases', verbose=True)
+# word2vec.word2phrase('text8', 'text8-phrases', verbose=True)
 
-word2vec.word2vec('train-phrases', 'train-100.bin', size=100, verbose=True)
+# word2vec.word2vec('text8-phrases', 'text8.bin', size=100, verbose=True)
 
 # word2vec.word2clusters('text8', 'text8-clusters.txt', 10, verbose=True)
+
+word2vec.word2phrase('enwik9', 'enwik9-phrases', verbose=True)
+
+word2vec.word2vec('enwik9-phrases', 'enwik9.bin', size=100, verbose=True)
